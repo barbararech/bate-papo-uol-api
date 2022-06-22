da43f1481488c82b99e9745af11f397cc2259af2
barbararech <rech.brb@gmail.com>
2022-06-22 10:15:50 -0300

    feat: add endpoint post participants

--- index.js
+++ index.js
@@ -1,9 +1,50 @@
-import express, {json} from "express";
+import express, { json } from "express";
 import cors from "cors";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+import dayjs from "dayjs";
 
+dotenv.config();
 const app = express();
 app.use(cors());
 app.use(json());
 
+const mongoClient = new MongoClient(process.env.URL_MONGO);
+let db;
 
-app.listen(5000);
+mongoClient.connect().then(() => {
+  db = mongoClient.db("bate-papo-uol");
+});
+
+app.post("/participants", (req, res) => {
+  const { name } = req.body;
+
+  db.collection("users")
+    .insertOne({
+      name:  name ,
+      lastStatus: Date.now(),
+    })
+    .catch(() => res.sendStatus(500));
+
+  db.collection("messages")
+    .insertOne({
+      from: name,
+      to: "Todos",
+      text: "entra na sala...",
+      type: "status",
+      time: dayjs().format('HH:mm:ss'),
+    })
+    .catch(() => res.sendStatus(500));
+
+    res.sendStatus(201);
+});
+
+app.get("/participants", (req, res) => {});
+
+app.post("/messages", (req, res) => {});
+
+app.get("/messages", (req, res) => {});
+
+app.post("/status", (req, res) => {});
+
+app.listen(5000, () => console.log("Server On!"));
