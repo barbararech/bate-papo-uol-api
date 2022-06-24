d53caa00502c9e9eaf58a730d2f652682b7c8562
barbararech <rech.brb@gmail.com>
2022-06-24 10:35:21 -0300

    feat: add data validation with joi library

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import dayjs from "dayjs";
+import joi from "joi";
 
 dotenv.config();
 const app = express();
@@ -16,14 +17,40 @@ mongoClient.connect().then(() => {
   db = mongoClient.db("bate-papo-uol");
 });
 
+const participantSchema = joi.object({
+  name: joi.string().required(),
+});
+
+const messageSchema = joi.object({
+  to: joi.string().required(),
+  text: joi.string().required(),
+  type: joi.string().valid("message", "private_message").required(),
+});
+
 app.post("/participants", async (req, res) => {
   const { name } = req.body;
 
+  const validation = participantSchema.validate({ name }, { abortEarly: true });
+
+  if (validation.error) {
+    console.log(validation.error.details);
+    return res.sendStatus(422);
+  }
+
   try {
+    const participantExist = await db
+      .collection("users")
+      .findOne({ name: name });
+
+    if (participantExist) {
+      return res.sendStatus(409);
+    }
+
     await db.collection("users").insertOne({
       name: name,
       lastStatus: Date.now(),
     });
+
     await db.collection("messages").insertOne({
       from: name,
       to: "Todos",
@@ -31,6 +58,7 @@ app.post("/participants", async (req, res) => {
       type: "status",
       time: dayjs().format("HH:mm:ss"),
     });
+
     res.sendStatus(201);
   } catch (error) {
     res.sendStatus(500);
@@ -50,7 +78,24 @@ app.post("/messages", async (req, res) => {
   const { to, text, type } = req.body;
   const from = req.header("user");
 
+  const validation = messageSchema.validate(req.body, {
+    abortEarly: false,
+  });
+
+  if (validation.error) {
+    console.log(validation.error.details);
+    return res.sendStatus(422);
+  }
+
   try {
+    const participantExist = await db
+      .collection("users")
+      .findOne({ name: from });
+
+    if (!participantExist) {
+      return res.sendStatus(422);
+    }
+
     await db.collection("messages").insertOne({
       from,
       to,
@@ -118,9 +163,8 @@ setInterval(async () => {
 
     users.map(async (user) => {
       const { lastStatus, name } = user;
-
       if (timeNow - lastStatus > 10000) {
-        await db.collection("users").deleteOne({ lastStatus });
+        await db.collection("users").deleteOne({ name });
         await db.collection("messages").insertOne({
           from: name,
           to: "Todos",
@@ -128,7 +172,7 @@ setInterval(async () => {
           type: "status",
           time: dayjs().format("HH:mm:ss"),
         });
-        return console.log("Usuário não está mais ativo");
+        return console.log(`${name} não está mais ativo`);
       }
     });
   } catch (error) {
